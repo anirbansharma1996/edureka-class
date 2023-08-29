12b9a3ace170955f38a5f18524bbb75df8964d4b
Anirban Sharma <107566923+anirbansharma1996@users.noreply.github.com>
2023-08-29 19:07:41 +0530

    Add files via upload

--- API/news-app/script.js
+++ API/news-app/script.js
@@ -1,47 +1,92 @@
 const root = document.getElementById("root");
-const news_count = document.getElementById("news-count")
+const news_count = document.getElementById("news-count");
+const page_num = document.getElementById("page")
+const limit_num = document.getElementById('limit-result')
+let count = 1
+let limit=3
+
+limit_num.addEventListener('change',()=>{
+  limit=Number(limit_num.value)
+  getData()
+})
+
+const URL = "https://decan-mock-api-data.onrender.com/newsData";
 
 
-const URL = `https://decan-mock-api-data.onrender.com/newsData`
-
-async function getData (){
+async function getData() {
   try {
-     root.innerHTML=`Loading...`
-     let response = await fetch(URL) 
-     let data = await response.json() 
-     news_count.innerHTML=`You have ${data.length} News On ${new Date().toLocaleDateString()} `
-     root.innerHTML=""
-     showNews(data)
+    root.innerHTML = `Loading...`;
+    let response = await fetch(URL+"?_limit="+limit+"&_page="+count);  
+    // or, let response = await fetch(`${URL}?_limit=${limit}&_page=${count}`);
+    let data = await response.json();
+    news_count.innerHTML = `${
+      data.length
+    }  news on ${new Date().toLocaleDateString()} page ${count}`;
+    root.innerHTML = "";
+    showNews(data);
+    showPage()
   } catch (error) {
-    root.innerHTML="404 | Something Went Wrong"
-    console.log(error.message)
+    root.innerHTML = "404 | Something Went Wrong";
+    console.log(error.message);
   }
 }
-getData ()
-function showNews(data){
-  data.forEach((el,id)=>{
-    let main = document.createElement('div')
-    main.className="main"
-    let image = document.createElement('img')
-    image.src = el.image
-    let headline = document.createElement('h3')
-    headline.innerText = el.headline
-    let date = document.createElement('p')
-    date.innerText = el.date
-   let read= document.createElement('button')
-   read.innerText="Read More"
-   read.addEventListener('click',()=>{
-      ReadMoreNews(el)
+
+getData();
+function showNews(data) {
+  data.forEach((el, id) => {
+    let main = document.createElement("div");
+    main.className = "main";
+    let image = document.createElement("img");
+    image.src = el.image;
+    let headline = document.createElement("h3");
+    headline.innerText = el.headline;
+    let date = document.createElement("p");
+    date.innerText = el.date;
+    let read = document.createElement("button");
+    read.innerText = "Read More";
+    read.addEventListener("click", () => {
+      ReadMoreNews(el);
+    });
+
+    main.append(image, date, headline, read);
+    root.append(main);
+  });
+}
+
+function ReadMoreNews(news) {
+  localStorage.setItem("news", JSON.stringify(news));
+  window.location.href = "details.html";
+}
+
+//pagination section js
+
+function showPage(){
+   let div = document.createElement('div')
+   div.className='page_div'
+   
+   let previous = document.createElement('button')
+   previous.innerText='Previous'
+   previous.addEventListener('click',()=>{
+    if(count>1){
+      count--
+      getData()
+    }
    })
 
-    main.append(image,date,headline,read)
-    root.append(main)
-  })
-}
+   let page_count = document.createElement("h4")
+   page_count.innerText=count
 
-function ReadMoreNews(news){
-  localStorage.setItem("news",JSON.stringify(news))
-  window.location.href="details.html"
-}
 
+   let next = document.createElement('button')
+   next.innerText='Next'
+   next.addEventListener('click',()=>{
+     count++
+     getData()
+   })
+
+   div.append(previous,page_count,next)
+   page_num.innerHTML=""
+   page_num.append(div)
+
+}
 
